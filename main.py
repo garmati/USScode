7ff2793278bdbee9c09ebdd014f51db0653b5653
garmati <133597806+garmati@users.noreply.github.com>
2023-07-11 19:56:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 import numpy as np
 
-# Data
+
 foil = [6.6, 6.2, 5.8, 6, 6, 4, 5.9, 5.9, 4.1, 4.2, 5.85, 4.15, 4.15, 4.15]
 jnd = [-1, -1, -1, 0.8, 0.8, 0.8, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.85]
+colors = [2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 2]
 
-# Convert the lists to numpy arrays
+
 X = np.array(foil).reshape(-1, 1)
-y = np.array(jnd)
+y_jnd = np.array(jnd)
+y_color = np.array(colors)
 
-# Create and train the Random Forest Regressor
+
 regressor = RandomForestRegressor()
-regressor.fit(X, y)
+regressor.fit(X, y_jnd)
 
-# Predict the JND for a new foil value
-new_foil = 4.5  # Replace with the actual new foil value
-predicted_jnd = regressor.predict([[new_foil]])
+
+classifier = RandomForestClassifier()
+classifier.fit(X, y_color)
+
+
+new_foil = np.array([4.5, 5.0, 5.5])
+predicted_jnd = regressor.predict(new_foil.reshape(-1, 1))
+predicted_color = classifier.predict(new_foil.reshape(-1, 1))
 
 print("Predicted JND:", predicted_jnd)
+print("Predicted Response:", predicted_color)
+
 
 
 
